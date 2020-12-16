48fea71b4c8b05e301e39dc661f7902d8106ebc0
Paola <paolasalazar90@gmail.com>
2020-12-16 14:37:32 -0300

    cuento 1

--- src/views/Level1.vue
+++ src/views/Level1.vue
@@ -2,11 +2,19 @@
   <div class="background">
       <Navbar/>
       <div class="container">
-      <h1 class="text-center display-2">Level 1</h1>
-      <h2 class="text-center display-3">The Tortoise and the Hare</h2>
-      <p>
-         Once upon a time there was a hare who spent all day bragging about how fast he could run. Tired of hearing him boast, the tortoise, challenged him to a race.<br>“You must be kidding!” said the hare laughing. “I am so much faster than you.”<br>“We shall see,” the tortoise replied.<br>The next day, the animals of the forest gathered to watch the race. Everyone wanted to see if the tortoise could beat the hare.The bear started the race yelling “On you mark, get set, go!”<br>The hare immediately raced ahead, running faster than ever. He looked back and saw the tortoise was only a few steps away from the starting line.<br>“Foolish tortoise, ” thought the hare. “He is so slow. Why would he want to race me if he has no chance to win?” Confident that he was going to win the race, the hare decided to stop in the middle of the road to rest under a tree. The cool and pleasant shade of the tree was very relaxing, so much so that the hare fell asleep.<br>Meanwhile, the tortoise continued walking slowly, but steady. He was determined not to give up. Soon, he found the hare sleeping peacefully.<br>The tortoise was winning the race! When the tortoise approached the finish line, all the animals in the forest began cheering with excitement. The noise woke the hare, who could not believe his eyes: the tortoise was crossing the finish line and he had lost the race. 
-      </p>
+          <div v-if="!show">
+            <h1 class="text-center display-2">Level 1</h1>
+            <h2 class="text-center display-3">The Tortoise and the Hare</h2>
+            <p>
+                Once upon a time there was a hare who spent all day bragging about how fast he could run. Tired of hearing him boast, the tortoise, challenged him to a race.<br>“You must be kidding!” said the hare laughing. “I am so much faster than you.”<br>“We shall see,” the tortoise replied.<br>The next day, the animals of the forest gathered to watch the race. Everyone wanted to see if the tortoise could beat the hare.The bear started the race yelling “On you mark, get set, go!”<br>The hare immediately raced ahead, running faster than ever. He looked back and saw the tortoise was only a few steps away from the starting line.<br>“Foolish tortoise, ” thought the hare. “He is so slow. Why would he want to race me if he has no chance to win?” Confident that he was going to win the race, the hare decided to stop in the middle of the road to rest under a tree. The cool and pleasant shade of the tree was very relaxing, so much so that the hare fell asleep.<br>Meanwhile, the tortoise continued walking slowly, but steady. He was determined not to give up. Soon, he found the hare sleeping peacefully.<br>The tortoise was winning the race! When the tortoise approached the finish line, all the animals in the forest began cheering with excitement. The noise woke the hare, who could not believe his eyes: the tortoise was crossing the finish line and he had lost the race. 
+            </p>
+          </div>
+          <div class="text-center">
+            <b-button @click="quiz" size="lg" class="my-5" variant="light">Go to quiz</b-button>
+          </div>
+      <div v-if="show">
+          
+      </div>
 
       </div>
   </div>
@@ -18,7 +26,17 @@ export default {
     name: 'Level1',
     components: {
         Navbar
-    }
+    },
+    data() {
+        return {
+            show: false,
+        }
+    },
+    methods: {
+        quiz(){
+            this.show= true
+        }
+    },
 }
 </script>
 
