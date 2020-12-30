9328285eb3b637cc81b26a23d31b53b11a0dfd93
Manuel Sayago <manuelsayago15@gmail.com>
2020-12-30 13:28:08 -0300

    Adding firebase configuration

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export var firebaseConfig = {
+    apiKey: "AIzaSyAcO6d_YyJPQcre1Kf5a7hyWOij-ESYHFc",
+    authDomain: "save-the-word-a1baa.firebaseapp.com",
+    projectId: "save-the-word-a1baa",
+    storageBucket: "save-the-word-a1baa.appspot.com",
+    messagingSenderId: "206679406760",
+    appId: "1:206679406760:web:5ef4a0c30d14488124aa9d"
+};
--- src/main.js
+++ src/main.js
@@ -8,11 +8,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import firebase from 'firebase'
+import { firebaseConfig } from '@/config/firebaseConfig'
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
